58d76c961a40515dc7bdc602fce62a0475b9bdcd
Adam <mr.laszczewski@gmail.com>
2024-09-20 22:29:06 +0200

    Initial

---
+++ src/app/app.config.ts
@@ -0,0 +1,16 @@
+import { ApplicationConfig } from "@angular/core";
+import { provideRouter } from "@angular/router";
+
+import { routes } from "./app.routes";
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+import { withNgxsReduxDevtoolsPlugin } from "@ngxs/devtools-plugin";
+import { withNgxsLoggerPlugin } from "@ngxs/logger-plugin";
+import { provideStore } from "@ngxs/store";
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideStore([], withNgxsReduxDevtoolsPlugin(), withNgxsLoggerPlugin()),
+  ],
+};
